5094b1d48ee90c7d391bd5934daaf25d55180abe
qian10 <qianjywuxi@gmail.com>
2023-09-11 23:58:31 +0800

    New feature: Users can now follow each other

---
+++ src/test/java/com/nowcoder/community/SensitiveTests.java
@@ -0,0 +1,24 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.util.SensitiveFilter;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class SensitiveTests {
+
+    @Autowired
+    private SensitiveFilter sensitiveFilter;
+
+    @Test
+    public void testSensitiveFilter() {
+        String text = "赌博 开开开 开票";
+        text = sensitiveFilter.filter(text);
+        System.out.println(text);
+    }
+
+
+}
